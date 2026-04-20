14bcdaddcf29ca60da8a3689c8cce7856f450cc0
feligonzalezr-ctrl <feli.gonzalezr@duocuc.cl>
2026-04-20 16:32:24 -0400

    Add files via upload

---
+++ the_game/src/main/java/com/duoc/the_game/TheGameApplication.java
@@ -0,0 +1,13 @@
+package com.duoc.the_game;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TheGameApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TheGameApplication.class, args);
+	}
+
+}
---
+++ the_game/src/test/java/com/duoc/the_game/TheGameApplicationTests.java
@@ -0,0 +1,13 @@
+package com.duoc.the_game;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TheGameApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
