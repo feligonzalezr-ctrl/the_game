825895edb75d5f0f58b31e1e8090b250cfd30c36
feligonzalezr-ctrl <feli.gonzalezr@duocuc.cl>
2026-04-20 17:41:53 -0400

    nos cambiamo a inteli

---
+++ the_game/src/main/java/com/duoc/the_game/model/inventario.java
@@ -0,0 +1,4 @@
+package com.duoc.the_game.model;
+
+public class inventario {
+}
---
+++ the_game/src/main/java/com/duoc/the_game/model/juego.java
@@ -0,0 +1,20 @@
+package com.duoc.the_game.model;
+
+public class juego {
+
+
+        //Atributos Juegos
+
+        private String nombre;
+        private String codigo;
+        private String categoria;
+        private String dificultad;
+        private int cantidadminimadejugadores;
+        private int cantidadmaximadejugadores;
+        private int duracionminima;
+        private int duracionmaxima;
+        private String jugabilidad;
+        private String tags;
+        private String contenido;
+
+    }
