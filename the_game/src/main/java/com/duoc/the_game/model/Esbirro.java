da6d723829f774c18487dda7cd1b372e1090f079
BrayanJorquera <br.jorquera@duocuc.cl>
2026-04-27 17:52:52 -0400

    Implementación de Lombok

--- the_game/src/main/java/com/duoc/the_game/model/Esbirro.java
+++ the_game/src/main/java/com/duoc/the_game/model/Esbirro.java
@@ -1,71 +1,25 @@
 package com.duoc.the_game.model;
+import jakarta.persistence.*;
+import lombok.*;
 
+@Entity
+@Table(name = "esbirros")
+@NoArgsConstructor @AllArgsConstructor //Añade los metodos Constructor
+@Getter @Setter //añade los metodos Getter & Setter
+@ToString //añade el metodo ToString
 public class Esbirro {
     //Atributos
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
+    @Column(nullable = false)
     private String run;
+    @Column(nullable = false)
     private String nombre;
+    @Column(nullable = false)
     private String nick;
+    @Column(nullable = false)
     private int edad;
+    @Column(nullable = false)
     private String Disponibilidad;
-
-    //Constructor
-    public Esbirro(){}
-    public Esbirro(String disponibilidad, int edad, int id, String nick, String nombre, String run) {
-        Disponibilidad = disponibilidad;
-        this.edad = edad;
-        this.id = id;
-        this.nick = nick;
-        this.nombre = nombre;
-        this.run = run;
-    }
-
-    //Getter & Setter
-    public String getDisponibilidad() {
-        return Disponibilidad;
-    }
-
-    public void setDisponibilidad(String disponibilidad) {
-        Disponibilidad = disponibilidad;
-    }
-
-    public int getEdad() {
-        return edad;
-    }
-
-    public void setEdad(int edad) {
-        this.edad = edad;
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getNick() {
-        return nick;
-    }
-
-    public void setNick(String nick) {
-        this.nick = nick;
-    }
-
-    public String getNombre() {
-        return nombre;
-    }
-
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
-    }
-
-    public String getRun() {
-        return run;
-    }
-
-    public void setRun(String run) {
-        this.run = run;
-    }
 }
--- the_game/src/main/java/com/duoc/the_game/model/Juego.java
+++ the_game/src/main/java/com/duoc/the_game/model/Juego.java
@@ -1,121 +1,35 @@
 package com.duoc.the_game.model;
+import jakarta.persistence.*;
+import lombok.*;
 
+@Entity
+@Table(name = "juegos")
+@NoArgsConstructor @AllArgsConstructor //Añade los metodos Constructor
+@Getter @Setter //añade los metodos Getter & Setter
+@ToString //añade el metodo ToString
 public class Juego {
         //Atributos Juegos
+        @Id
+        @GeneratedValue(strategy = GenerationType.IDENTITY)
+        private int id;
+        @Column(nullable = false)
         private String nombre;
-        private String codigo;
+        @Column(nullable = false)
         private String categoria;
+        @Column(nullable = false)
         private String dificultad;
+        @Column(nullable = false)
         private int cantidadminimadejugadores;
+        @Column(nullable = false)
         private int cantidadmaximadejugadores;
+        @Column(nullable = false)
         private int duracionminima;
+        @Column(nullable = false)
         private int duracionmaxima;
+        @Column(nullable = false)
         private String jugabilidad;
+        @Column(nullable = true)
         private String tags;
+        @Column(nullable = false)
         private String contenido;
-
-        //Constructor
-        public Juego(){}
-        public Juego(int cantidadmaximadejugadores, int cantidadminimadejugadores, String categoria, String codigo, String contenido, String dificultad, int duracionmaxima, int duracionminima, String jugabilidad, String nombre, String tags) {
-                this.cantidadmaximadejugadores = cantidadmaximadejugadores;
-                this.cantidadminimadejugadores = cantidadminimadejugadores;
-                this.categoria = categoria;
-                this.codigo = codigo;
-                this.contenido = contenido;
-                this.dificultad = dificultad;
-                this.duracionmaxima = duracionmaxima;
-                this.duracionminima = duracionminima;
-                this.jugabilidad = jugabilidad;
-                this.nombre = nombre;
-                this.tags = tags;
-        }
-
-        //Getter & Setter
-        public int getCantidadmaximadejugadores() {
-                return cantidadmaximadejugadores;
-        }
-
-        public void setCantidadmaximadejugadores(int cantidadmaximadejugadores) {
-                this.cantidadmaximadejugadores = cantidadmaximadejugadores;
-        }
-
-        public int getCantidadminimadejugadores() {
-                return cantidadminimadejugadores;
-        }
-
-        public void setCantidadminimadejugadores(int cantidadminimadejugadores) {
-                this.cantidadminimadejugadores = cantidadminimadejugadores;
-        }
-
-        public String getCategoria() {
-                return categoria;
-        }
-
-        public void setCategoria(String categoria) {
-                this.categoria = categoria;
-        }
-
-        public String getCodigo() {
-                return codigo;
-        }
-
-        public void setCodigo(String codigo) {
-                this.codigo = codigo;
-        }
-
-        public String getContenido() {
-                return contenido;
-        }
-
-        public void setContenido(String contenido) {
-                this.contenido = contenido;
-        }
-
-        public String getDificultad() {
-                return dificultad;
-        }
-
-        public void setDificultad(String dificultad) {
-                this.dificultad = dificultad;
-        }
-
-        public int getDuracionmaxima() {
-                return duracionmaxima;
-        }
-
-        public void setDuracionmaxima(int duracionmaxima) {
-                this.duracionmaxima = duracionmaxima;
-        }
-
-        public int getDuracionminima() {
-                return duracionminima;
-        }
-
-        public void setDuracionminima(int duracionminima) {
-                this.duracionminima = duracionminima;
-        }
-
-        public String getJugabilidad() {
-                return jugabilidad;
-        }
-
-        public void setJugabilidad(String jugabilidad) {
-                this.jugabilidad = jugabilidad;
-        }
-
-        public String getNombre() {
-                return nombre;
-        }
-
-        public void setNombre(String nombre) {
-                this.nombre = nombre;
-        }
-
-        public String getTags() {
-                return tags;
-        }
-
-        public void setTags(String tags) {
-                this.tags = tags;
-        }
 }
