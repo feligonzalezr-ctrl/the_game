d6bc808b6e533a5b4c7bdcaa0fca37b89cc6611b
BrayanJorquera <br.jorquera@duocuc.cl>
2026-05-11 17:35:25 -0400

    Añadido dto a service

--- juego-service/src/main/java/com/the_game/juego_service/service/JuegoService.java
+++ juego-service/src/main/java/com/the_game/juego_service/service/JuegoService.java
@@ -1,8 +1,11 @@
 package com.the_game.juego_service.service;
 import com.the_game.juego_service.model.Juego;
+import com.the_game.juego_service.dto.JuegoRequestDTO;
+import com.the_game.juego_service.dto.JuegoResonseDTO;
 import com.the_game.juego_service.repository.JuegoRepository;
 import org.springframework.stereotype.Service;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class JuegoService {
@@ -11,8 +14,43 @@ public class JuegoService {
         this.juegoRepository = juegoRepository;
     }
 
-    public List<Juego> listarJuegos() {
-        return juegoRepository.findAll();
+    public List<JuegoResponseDTO> listarJuegos() {
+        return juegoRepository.findAll().stream().map(this::convertirAResponseDTO).collect(Collectors.toList());
+    }
+
+    public JuegoResponseDTO buscarPorId(int){
+        Juego juego = new juegoRepositorty.findById(id).orElse(null);
+        if(juego=null){return null}
+        return convertirAResponseDTO(juego)
+    }
+
+    public JuegoResponseDTO guardar(JuegoRequestDTO dto){
+        Juego juego =new Juego();
+        juego.setNombre(dto.getNombre());
+        juego.setCategoria(dto.getCategoria());
+        juego.setCantidadMinimaJugadores(dto.getCantidadMinimaJugadores());
+        juego.setCantidadMaximaJugadores(dto.getCantidadMaximaJugadores());
+        juego.setContenido(dto.getContenido());
+        juego.setDificultad(dto.getDificultad());
+        juego.setDuracionMinima(dto.getDuracionMinima());
+        juego.setDuracionMaxima(dto.getDuracionMaxima());
+        juego.setJugabilidad(dto.getJugabilidad());
+
+        Juego guardado= new juegoRepository.save(juego);
+        return convertirAResponseDTO(guardado);
+    }
+
+    private JuegoResponseDTO convertirAResponseDTO(Juego juego){
+        return new JuegoResponseDTO(
+                juego.getNombre(),
+                juego.getCategoria(),
+                juego.getCantidadMinimaJugadores(),
+                juego.getCantidadMaximaJugadores(),
+                juego.getContenido(),
+                juego.getDificultad(),
+                juego.getDuracionMinima(),
+                juego.getDuracionMaxima(),
+                juego.getJugabilidad());
     }
 
     public Juego agregarJuego(Juego juego) {
