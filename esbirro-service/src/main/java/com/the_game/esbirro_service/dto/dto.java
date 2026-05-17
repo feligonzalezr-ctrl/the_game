eb41970c299ae44d6ee66800c7d12679aabc69bf
Felipe <feli.gonzalerz@duocuc.cl>
2026-05-17 19:29:58 -0400

    agrege dto a esbirro-service

---
+++ esbirro-service/src/main/java/com/the_game/esbirro_service/dto/dto.java
@@ -0,0 +1,15 @@
+package com.the_game.esbirro_service.dto;
+
+
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class dto {
+    private Long id;
+    private String nombre;
+    private String rol;
+    private int nivel;
+}
