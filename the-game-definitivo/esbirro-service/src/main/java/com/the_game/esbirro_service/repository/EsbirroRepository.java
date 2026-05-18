bc5e1ee5c97477df19bab795c76d571d0f292455
feligonzalezr-ctrl <feli.gonzalezr@duocuc.cl>
2026-05-17 22:00:27 -0400

    Delete the-game-definitivo directory

--- the-game-definitivo/esbirro-service/src/main/java/com/the_game/esbirro_service/repository/EsbirroRepository.java
+++
@@ -1,6 +0,0 @@
-package com.the_game.esbirro_service.repository;
-
-import com.the_game.esbirro_service.model.Esbirro;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface EsbirroRepository extends JpaRepository<Esbirro, Integer> { }
--- the-game-definitivo/inventario-service/src/main/java/com/the_game/inventario_service/repository/InventarioRepository.java
+++
@@ -1,7 +0,0 @@
-package com.the_game.inventario_service.repository;
-
-import com.the_game.inventario_service.model.Inventario;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface InventarioRepository extends JpaRepository<Inventario,Long> {}
-
